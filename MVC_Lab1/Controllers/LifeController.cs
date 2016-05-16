52b601a408e4993e06642af62cb85d5baaff4913
RamiGranat <raextheassassin@gmail.com>
2016-05-16 15:33:45 +0200

    GAZE UPON THE LABS!

---
+++ MVC_Lab1/Controllers/LifeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Lab1.Controllers
+{
+    public class LifeController : Controller
+    {
+        // GET: Life
+        public ActionResult SuperMario()
+        {
+            return View();
+        }
+        public ActionResult Health()
+        {
+            return View();
+        }
+        public ActionResult Live(int? id)
+        {
+            if (id !=null)
+            {
+                ViewBag.Amount = id;
+            }
+            else
+            {
+                ViewBag.Amount = "";
+            }
+            return View();
+        }
+    }
+}
